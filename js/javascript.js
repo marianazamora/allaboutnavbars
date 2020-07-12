37f6fa13cd9e806e31333227d4c68ef133212784
Mariana Perez Zamora <64731365+marianazamora@users.noreply.github.com>
2020-07-13 00:37:15 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+//Function for navigationbar (Tutorial from w3schools)
+function popnavigation() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
